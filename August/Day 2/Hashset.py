5c296949c3553600e55df316e455462aa84fe851
praneeth95 <praneeth95@gmail.com>
2020-08-02 19:28:06 +0530

    Day2

---
+++ August/Day 2/Hashset.py
@@ -0,0 +1,50 @@
+class MyHashSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 10000
+        self.table = [None]*self.size
+
+    def calculate_hash_value(self,key):
+        return key % self.size
+
+    def add(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        hv = self.calculate_hash_value(key)
+        if self.table[hv] == None:
+            self.table[hv] = [key]
+        else:
+            self.table[hv].append(key)
+
+
+    def remove(self, key):
+        """
+        :type key: int
+        :rtype: None
+        """
+        hv = self.calculate_hash_value(key)
+        if self.table[hv] != None:
+            while key in self.table[hv]:
+                self.table[hv].remove(key)
+
+    def contains(self, key):
+        """
+        Returns true if this set contains the specified element
+        :type key: int
+        :rtype: bool
+        """
+        hv = self.calculate_hash_value(key)
+        if self.table[hv] != None:
+            if key in self.table[hv]:
+                return True
+            return False
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
