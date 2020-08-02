0a59ac87b7f0c260c0e58ae30c7cf5f2a569bc10
praneeth95 <praneeth95@gmail.com>
2020-08-02 12:12:38 +0530

    August Solutions:q

--- August/Day 1/Test.js
+++ August/Day 1/Test.js
@@ -3,6 +3,15 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-  word ="Hello World";
-    return word;  
+    if (word===word.toUpperCase()) {
+            return true;
+        }
+        var len = word.length;
+        for (var i = 1; i < word.length; i++) {
+            var ch = word.charAt(i);
+            if ('A' <= ch  && ch <= 'Z') { 					//[A-Z = 65-90]  and [a-z = 97-122] = leetcode, United
+                return false;
+            }
+        }
+        return true;
 };
