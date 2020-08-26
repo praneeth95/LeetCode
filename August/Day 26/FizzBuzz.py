03f7d294fbb7396c7afdef2b7f7a1ec3daf6a60a
praneeth95 <praneeth95@gmail.com>
2020-08-26 15:07:37 +0530

    FizzBuzz

---
+++ August/Day 26/FizzBuzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        res=[]
+        if n>0:
+            for i in range(1,n+1):
+                if i % 3 == 0 and i % 5 ==0:
+                    res.append("FizzBuzz")
+                elif i % 5 == 0:
+                    res.append("Buzz")
+                elif i % 3 == 0:
+                    res.append("Fizz")
+                else:
+                    res.append(str(i))
+            print(res)
+            return res
